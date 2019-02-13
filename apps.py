9c21d33226136f122ce629200aa023711adc078c
ochera <47291622+ochera@users.noreply.github.com>
2019-02-13 01:47:46 -0800

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PaggesConfig(AppConfig):
+    name = 'pagges'
---
+++ views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def home(request):
+	return render(request, "home.html", {})
+
+
+def about(request):
+	return render(request, "about.html", {})	
+
+def contact(request):
+	return render(request, "contact.html", {})
+
+def register(request):
+	return render(request, "register.html", {})
